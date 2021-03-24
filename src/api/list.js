75a20b1a65829d7ef668adab52d7edfb52b30559
nicolesurawski <nicolesurawski@gmail.com>
2021-03-24 12:04:42 -0400

    Add List actions axios calls & Update Header

---
+++ src/api/list.js
@@ -0,0 +1,36 @@
+import apiUrl from '../apiConfig'
+import axios from 'axios'
+
+// CREATE
+export const createListItem = (list, user) => {
+  return axios({
+    url: apiUrl + '/lists',
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${user.token}`
+    },
+    data: { list: list }
+  })
+}
+
+// SHOW user list
+export const listIndexUser = user => {
+  return axios({
+    url: apiUrl + '/lists',
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${user.token}`
+    }
+  })
+}
+
+// DELETE list item
+export const listItemDelete = (id, user) => {
+  return axios({
+    url: apiUrl + '/lists/' + id,
+    method: 'DELETE',
+    headers: {
+      'Authorization': `Bearer ${user.token}`
+    }
+  })
+}
